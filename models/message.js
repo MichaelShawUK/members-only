d2d70d5b1baff377c4750bc3f3b548c7aa7e2d02
MichaelShawUK <MichaelShawUK@gmail.com>
2023-04-10 13:35:37 +0100

    Format message timestamp

--- models/message.js
+++ models/message.js
@@ -5,7 +5,19 @@ const MessageSchema = new Schema({
   title: { type: String, required: true, maxLength: 50 },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   body: { type: String, required: true, maxLength: 200 },
-  time: { type: Date, default: Date.now },
+  time: { type: Date, default: new Date() },
+});
+
+MessageSchema.virtual("timestamp").get(function () {
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  };
+
+  return this.time.toLocaleDateString("en-GB", options);
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
