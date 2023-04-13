08e64ba89e56919e05befb85fc997fdb6a294e06
MichaelShawUK <MichaelShawUK@gmail.com>
2023-04-13 12:18:17 +0100

    Dashboard message view responds to narrow screen width

--- models/user.js
+++ models/user.js
@@ -1,11 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const colour = function () {
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = 70 + Math.floor(Math.random() * 30);
+  const lightness = 40 + Math.floor(Math.random() * 20);
+
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+};
+
 const UserSchema = new Schema({
   username: { type: String, required: true, maxLength: 30 },
   password: { type: String, required: true },
   isMember: { type: Boolean, default: false },
   isAdmin: { type: Boolean, default: false },
+  colour: { type: String, default: colour },
 });
+
+// UserSchema.virtual("colour").get(function () {
+//   const hue = Math.floor(Math.random() * 360);
+//   const saturation = 70 + Math.floor(Math.random() * 30);
+//   const lightness = 40 + Math.floor(Math.random() * 20);
+
+//   return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+// });
 
 module.exports = mongoose.model("User", UserSchema);
